301eca9420ce5d3877b4d78b2725940ec562cc1c
jacob-bauer <152446301+jacob-bauer@users.noreply.github.com>
2023-12-11 14:55:46 -0700

    Created test project

---
+++ PerfectDecimal/PerfectDecimal-test/ConstructorTests.cs
@@ -0,0 +1,16 @@
+namespace PerfectDecimal_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
