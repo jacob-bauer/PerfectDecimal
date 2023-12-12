29de30e319c8c679835301a5fbc580a875de9a07
jacob-bauer <152446301+jacob-bauer@users.noreply.github.com>
2023-12-12 14:51:27 -0700

    Implemented tests for Object.Equals

--- PerfectDecimal/PerfectDecimal-test/ObjectOverrideTests.cs
+++ PerfectDecimal/PerfectDecimal-test/ObjectOverrideTests.cs
@@ -6,6 +6,127 @@ using System.Threading.Tasks;
 
 namespace PerfectDecimal_test
 {
+    internal class ObjectEqualsTests
+    {
+        [Test]
+        public void Value_Is_Null()
+        {
+            PerfectDecimal subject = new(1, 6);
+            object? test = null;
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Positive_Value_Is_Smaller_Fraction()
+        {
+            PerfectDecimal subject = new(1, 6);
+            object? test = new PerfectDecimal(1, 7);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Negative_Value_Is_Smaller_Fraction()
+        {
+            // Less than operator moves towards negative infinity, that is, -3 is less than -2
+            PerfectDecimal subject = new(-2, 3);
+            object? test = new PerfectDecimal(-3, 4);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Positive_Value_Is_Smaller_Whole()
+        {
+            PerfectDecimal subject = new(3, 1);
+            object? test = new PerfectDecimal(2, 1);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Negative_Value_Is_Smaller_Whole()
+        {
+            PerfectDecimal subject = new(-2, 1);
+            object? test = new PerfectDecimal(-3, 1);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Positive_Value_Is_Equal_Fraction()
+        {
+            PerfectDecimal subject = new(2, 3);
+            object? test = new PerfectDecimal(4, 6);
+
+            Assert.That(subject, Is.EqualTo(test));
+        }
+
+        [Test]
+        public void Negative_Value_Is_Equal_Fraction()
+        {
+            PerfectDecimal subject = new(-6, 4);
+            object? test = new PerfectDecimal(-12, 8);
+
+            Assert.That(subject, Is.EqualTo(test));
+        }
+
+        [Test]
+        public void Positive_Value_Is_Equal_Whole()
+        {
+            PerfectDecimal subject = new(2, 1);
+            object? test = new PerfectDecimal(4, 2);
+
+            Assert.That(subject, Is.EqualTo(test));
+        }
+
+        [Test]
+        public void Negative_Value_Is_Equal_Whole()
+        {
+            PerfectDecimal subject = new(-3, 1);
+            object? test = new PerfectDecimal(-9, 3);
+
+            Assert.That(subject, Is.EqualTo(test));
+        }
+
+        [Test]
+        public void Positive_Value_Larger_Fraction()
+        {
+            PerfectDecimal subject = new(1, 2);
+            object? test = new PerfectDecimal(3, 4);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Negative_Value_Larger_Fraction()
+        {
+            PerfectDecimal subject = new(-3, 4);
+            object? test = new PerfectDecimal(1, 2);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Positive_Value_Larger_Whole()
+        {
+            PerfectDecimal subject = new(5, 1);
+            object? test = new PerfectDecimal(6, 1);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+
+        [Test]
+        public void Negative_Value_Larger_Whole()
+        {
+            PerfectDecimal subject = new(-7, 1);
+            object? test = new PerfectDecimal(-6, 1);
+
+            Assert.That(subject, Is.Not.EqualTo(test));
+        }
+    }
+
     internal class ObjectGetHashCodeTests
     {
         [Test]
