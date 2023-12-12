b63463bcb8bbc4844363735cc16d25e3c9bd8bd5
jacob-bauer <152446301+jacob-bauer@users.noreply.github.com>
2023-12-12 10:54:19 -0700

    Implemented tests for IComparable<T>

--- PerfectDecimal/PerfectDecimal-test/ComparisonTests.cs
+++ PerfectDecimal/PerfectDecimal-test/ComparisonTests.cs
@@ -6,6 +6,81 @@ using System.Threading.Tasks;
 
 namespace PerfectDecimal_test
 {
+    internal class IComparableOfTTests
+    {
+        [Test]
+        public void Equal_Not_Reduced_Positive_Fraction()
+        {
+            PerfectDecimal subject = new(1, 2);
+            PerfectDecimal test = new(4, 8);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Equal_Not_Reduced_Positive_Whole()
+        {
+            PerfectDecimal subject = new(2, 1);
+            PerfectDecimal test = new(4, 2);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Equal_Not_Reduced_Negative_Fraction()
+        {
+            PerfectDecimal subject = new(-1, 2);
+            PerfectDecimal test = new(-4, 8);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Equal_Not_Reduced_Negative_Whole()
+        {
+            PerfectDecimal subject = new(-2, 1);
+            PerfectDecimal test = new(-8, 4);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Value_Is_Wrong_Type()
+        {
+            PerfectDecimal subject = new();
+            object? test = 2;
+
+            Assert.Throws<ArgumentException>(() => subject.CompareTo(test));
+        }
+
+        [Test]
+        public void Is_Larger()
+        {
+            PerfectDecimal subject = new();
+            PerfectDecimal test = new(-1, 1);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Is_Smaller()
+        {
+            PerfectDecimal subject = new();
+            PerfectDecimal test = new(1, 1);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Is_Equal()
+        {
+            PerfectDecimal subject = new(9, 7);
+            PerfectDecimal test = new(18, 14);
+
+            Assert.That(subject.CompareTo(test), Is.EqualTo(0));
+        }
+    }
+
     internal class IComparableTests
     {
         [Test]
